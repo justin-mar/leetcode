1e50f978037119095a52f9fc063002d05f753d06
justin-mar <70727597+justin-mar@users.noreply.github.com>
2026-01-30 23:33:22 -1000

    updates

--- Program.cs
+++ Program.cs
@@ -16,52 +16,97 @@ public class Program
         Solution sln = new Solution();
 
         // Input
-        int[] candidates = [10, 1, 2, 7, 6, 1, 5];
-        int target = 8;
+        int[][] isWater = [[0, 0, 1], [1, 0, 0], [0, 0, 0]];
 
-        IList<IList<int>> ans = sln.CombinationSum2(candidates, target);
+        int[][] ans = sln.HighestPeak(isWater);
         //Console.WriteLine();
-        //Console.Write(ans);
 
-        foreach (var i in ans)
+        for (int i = 0; i < ans.Length; i++)
         {
-            Console.Write("[");
-            foreach (int j in i)
+            foreach (int j in ans[i])
             {
-                Console.Write(j);
+                Console.Write(j + " ");
             }
-            Console.WriteLine("]");
+            Console.WriteLine();
         }
     }
 }
 
 public class Solution
 {
-    // For each subsequent number, we test either adding it or passing over it
-    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    /*
+     * Multi-source shortest path BFS
+     * Use water tiles a source for BFS
+     * If adjacent tile is not set then set its height as +1 then enqueue
+     * We start at each source and "ripple" out from there, thus there wouldn't be a tile with a height set that could be smaller as we would've reached it beforehand
+     */
+    public int[][] HighestPeak(int[][] isWater)
     {
-        IList<IList<int>> list = new List<IList<int>>();
-        Array.Sort(candidates);
-        Backtrack(list, new List<int>(), candidates, target, 0);
-        return list;
+        int m = isWater.Length;
+        int n = isWater[0].Length;
+
+        int[][] directions =
+        [
+            [-1, 0],      // Left
+            [1, 0],      // Right
+            [0, -1],      // Down
+            [0, 1],       // Up
+        ];
+
+
+        int[][] height = new int[m][];
+
+        // Set height's value to -1
+        for (int i = 0; i < m; ++i)
+        {
+            height[i] = new int[n];
+            Array.Fill(height[i], -1);
+        }
+
+        Queue<(int, int)> queue = new();
+
+        // Enqueue water tiles
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (isWater[i][j] == 1)
+                {
+                    height[i][j] = 0;
+                    queue.Enqueue((i, j));
+                }
+            }
+        }
+
+        while (queue.Count > 0)
+        {
+            var (x, y) = queue.Dequeue();
+
+            foreach (var dir in directions)
+            {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                // Check that newX and newY are in bound of height and value isn't decided
+                if (newX >= 0 && newX < m && newY  >= 0 && newY < n && height[newX][newY] == -1)
+                {
+                    height[newX][newY] = height[x][y] + 1;
+                    queue.Enqueue((newX, newY));
+                }
+            }
+        }
+
+        return height;
     }
 
-    private void Backtrack(IList<IList<int>> answer, IList<int> tempList, int[] candidates, int totalLeft, int index)
+    public void PrintQueue(Queue<(int, int)> queue)
     {
-        if (totalLeft == 0)
+        Console.WriteLine("Queue:");
+        foreach ((int, int) val in queue)
         {
-            answer.Add(new List<int>(tempList));
-            return;
+            Console.WriteLine(val.Item1 + " " + val.Item2);
         }
 
-        for (int i = index; i < candidates.Length; ++i)
-        {
-            if (i > index && candidates[i] == candidates[i - 1]) { continue; }      // Prevent reusing numbers
-            if (candidates[i] > totalLeft) { break; }
-
-            tempList.Add(candidates[i]);
-            Backtrack(answer, tempList, candidates, totalLeft - candidates[i], i + 1);      // Check for all possible combinations
-            tempList.RemoveAt(tempList.Count - 1);      // Backtrack tempList
-        }
+        Console.WriteLine();
     }
 }
--- problems/800-899/885-spiral-matrix-3.md
+++ problems/800-899/885-spiral-matrix-3.md
@@ -8,7 +8,10 @@ You will walk in a clockwise spiral shape to visit every position in this grid. 
 Return an array of coordinates representing the positions of the grid in the order you visited them.
 
 Input: rows = 1, cols = 4, rStart = 0, cStart = 0  
-Output: [[0,0],[0,1],[0,2],[0,3]]  
+Output: [[0,0],[0,1],[0,2],[0,3]]
+
+Input: rows = 5, cols = 6, rStart = 1, cStart = 4
+Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
 
 Constraints:
 * 1 <= rows, cols <= 100
